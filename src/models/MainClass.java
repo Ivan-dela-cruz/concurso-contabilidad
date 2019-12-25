000d5000d633a0329c7111a139cf6c54bcef3bd4
Ivan-dela_cruz <canarivan495@hotmail.com>
2019-12-25 08:21:55 -0500

    inicio del proyecto

---
+++ src/models/MainClass.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+import conexion.Conexion;
+
+/**
+ *
+ * @author ivan
+ */
+public class MainClass {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Conexion con = new Conexion();
+        con.conectar();
+    }
+    
+}
